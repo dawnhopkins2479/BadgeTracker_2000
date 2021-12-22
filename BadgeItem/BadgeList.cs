5f573d6df18b764798e392b6cb4c10ce0ae3d839
Dawn Hopkins <dawnhopkins2479@gmail.com>
2021-12-22 15:59:09 -0500

    First Commit

---
+++ BadgeItem/BadgeList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadgeItem
+{
+    class BadgeList
+    {
+        private Dictionary<int, Badge> _listOfBadges = new Dictionary<int, Badge>();
+        private Dictionary<int, Door> _listOfDoors = new Dictionary<int, Door>();
+        //Create
+        public bool CreateNewBadge(Badge badge)
+        {
+            bool bReturn = false;
+            int badgeCount = _listOfBadges.Count();
+            int id = GenerateUniqueId();
+            badge.BadgeId = id;
+
+            if (!_listOfBadges.ContainsKey(badge.BadgeId))
+            {
+                _listOfBadges.Add(badge.BadgeId, badge);
+                bReturn = true;
+            }
+            return bReturn;
+        }
+
+        public Badge GetBadgeById(int id)
+        {
+            return _listOfBadges[id];
+        }
+        public Dictionary<int, Badge> GetAllBadges()
+        {
+            return _listOfBadges;
+        }
+
+        private int GenerateUniqueId()
+        {
+            int badgeCount = _listOfBadges.Count;
+            return badgeCount + 1;
+        }
+        private int GenerateUniqueDoor()
+        {
+            int doorCount = _listOfDoors.Count;
+            return doorCount;
+        }
+    }
+}
---
+++ BadgeItem/Badges.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadgeItem
+{
+    public class Badge
+    {
+        public int BadgeId { get; set; }
+        public string Door { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NameForBadge => $"{FirstName} {LastName}";
+
+        public Badge(int badgeID, string door, string firstName, string lastName)
+        {
+            BadgeId = badgeID;
+            Door = door;
+            FirstName = firstName;
+            LastName = lastName;
+        }      
+    }
+    public class Door
+    {
+        public string DoorLetter { get; set; }
+        public int DoorNumber { get; set; }
+        public string DoorName => $"{DoorLetter} {DoorNumber}";
+
+        public Door(string doorLetter, int doorNumber)
+        {
+            DoorLetter = doorLetter;
+            DoorNumber = doorNumber;
+        }
+    }
+}
